c0a6100f97359cf01c726b6375a514202387a61a
Stephan Maready <stephan.maready@gmail.com>
2025-07-01 21:38:07 -0700

    Got rid of App CSS file, made a few changes to Header and Footer components, moved the Header and Footer components into a new Components folder

---
+++ mywebsite/src/Components/Footer.jsx
@@ -0,0 +1,10 @@
+
+function Footer() {
+    return(
+        <footer>
+            <p class="h-10 font-serif flex flex-row justify-center">&copy; {new Date().getFullYear} Stephan's Website</p>
+        </footer>
+    )
+}
+
+export default Footer
---
+++ mywebsite/src/Components/Header.jsx
@@ -0,0 +1,15 @@
+function Header() {
+    return(
+        <header>
+            <nav class="h-20 font-bold font-serif flex flex-row justify-center items-center w-full m-0 p-0 gap-x-32">
+                <a class="text-xl underline" href="#">Home</a>
+                <a class="text-xl underline" href="#">Projects</a>
+                <a class="text-xl underline" href="#">Contact</a>
+                <a class="text-xl underline" href="#">About Me</a>
+            </nav>
+            <hr/>
+        </header>
+    )
+}
+
+export default Header
